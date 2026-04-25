14072d5fe932124439088562ee4101d3b5bd9e9e
ifaakash <aakashc.appregis@gmail.com>
2026-04-25 19:31:13 +0530

    fix(LLMOps): change API endpoint from https to http

--- LLMOps/qwen-curl.sh
+++ LLMOps/qwen-curl.sh
@@ -2,7 +2,7 @@
 
 pi5_ip="100.76.6.76"
 ollama_port="11434"
-base_endpoint="https://$pi5_ip:$ollama_port/api/chat"
+base_endpoint="http://$pi5_ip:$ollama_port/api/chat"
 echo "Calling curl endpoint for qwen: "
 
 curl $base_endpoint \
